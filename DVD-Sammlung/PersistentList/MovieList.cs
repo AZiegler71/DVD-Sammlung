18440296492e8c703e9962d66534bc1b69ab6075
Alexander Ziegler <mail03@alex-ziegler.de>
2012-05-06 21:17:30 +0200

    Added folder.

---
+++ DVD-Sammlung/PersistentList/MovieList.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Linq;
+
+namespace DvdCollection
+{
+    class MovieList : ObservableCollection<MovieInfo>
+    {
+    }
+}
