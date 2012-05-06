4e37d30f6aa942a996b6943656d8de7f6ddda2af
Alexander Ziegler <mail03@alex-ziegler.de>
2012-05-06 21:16:57 +0200

    Added Firebird Client, database is created if not existing.

--- DVD-Sammlung/MoviePersistence.cs
+++ DVD-Sammlung/MoviePersistence.cs
@@ -1,4 +1,9 @@
 ﻿using System.Collections.Generic;
+using FirebirdSql.Data.FirebirdClient;
+using System.Collections;
+using System.Data;
+using System;
+using System.IO;
 
 namespace DvdCollection
 {
@@ -13,7 +18,66 @@ namespace DvdCollection
 
         public static List<MovieInfo> LoadMovies ()
         {
+            if (!File.Exists (DB_FILENAME))
+            {
+                CreateDatabase ();
+            }
+
+            //...
+
             return new List<MovieInfo> ();
         }
+
+        private static void CreateDatabase ()
+        {
+            string connectionString = string.Format (CONNECTION_STRING_FORMAT_FIREBIRD, DB_FILENAME);
+            FbConnection.CreateDatabase (connectionString);
+
+            using (FbConnection connection = new FbConnection (connectionString))
+            {
+                //connection.Open ();
+                RunStatements (connection, GetTableCreateStatementList ());
+            }
+        }
+
+        private static void RunStatements (FbConnection connection, IList<string> statements)
+        {
+            using (FbTransaction transaction = connection.BeginTransaction ())
+            {
+                foreach (string statement in statements)
+                {
+                    using (FbCommand cmd = new FbCommand (statement, connection, transaction))
+                    {
+                        cmd.ExecuteNonQuery ();
+                    }
+                }
+                transaction.Commit ();
+            }
+        }
+        private static List<string> GetTableCreateStatementList ()
+        {
+
+            List<string> statements = new List<string> ();
+
+            statements.Add (@"CREATE TABLE MOVIE_DATA (
+                                ID INTEGER,
+                                TITLE VARCHAR(200) NOT NULL,
+                                GENRES VARCHAR(300),
+                                DESCRIPTION VARCHAR(1000),
+                                RATING VARCHAR(200), 
+                                COVER BLOB,
+                                DVD VARCHAR(100) NOT NULL,
+                                FILE_DATA_X INTEGER NOT NULL,
+                                FILE_DATA_Y INTEGER NOT NULL,
+                                FILE_DATA_DURATION FLOAT NOT NULL,
+                                DB_RELEVANT_TITLE VARCHAR(200) NOT NULL,
+                                PRIMARY KEY (ID)
+                            );"
+                );
+            return statements;
+        }
+
+        private static readonly string CONNECTION_STRING_FORMAT_FIREBIRD = "User=u;Password=p;Database={0};Port=3050;Dialect=3;Charset=NONE;Role=;Connection lifetime=15;Pooling=true;MinPoolSize=0;MaxPoolSize=50;Packet Size=8192;ServerType=1;";
+        private static readonly string DB_FILENAME = "dvd.fdb";
     }
 }
